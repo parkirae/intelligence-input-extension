0731b0a000c263d46ce87d3d3d7ab16f795f9e51
parkirae <parkirae@kakao.com>
2023-06-11 16:45:22 +0900

    Fix: input name 속성 제거, id 속성 추가

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(function () {
+
   // input에 4자리 이상 입력했다면 다음 input으로 이동
-  $("input").on("input", function () {
+  $("input[id=input1], input[id=input2], input[id=input3], input[id=input4]").on("input", function () {
     let inputLength = $(this).val().length;
     let maxLength = 4;
 
@@ -9,16 +10,17 @@ $(function () {
     }
   });
 
+  // 수정 금지
   // 붙여넣기 유효성 검사에 사용되는 input index값
   // input에 focus가 되면
-  $("input").focus(function () {
+  $("input[id=input1], input[id=input2], input[id=input3], input[id=input4]").focus(function () {
     // this로 값을 가져와서
     let focusedInput = $(this);
     // +1을 해서 inputIndex에 저장
     inputIndex = $("input").index(focusedInput) + 1;
   });
 
-  $("input").keydown(function (e) {
+  $("input[id=input1], input[id=input2], input[id=input3], input[id=input4]").keydown(function (e) {
     // ctrl 누르면 함수 실행 중단 및 종료
     if (e.ctrlKey) return;
     if (e.altKey) return;
@@ -35,7 +37,7 @@ $(function () {
   });
 
   // 붙여넣기 막기
-  $("input").on("paste", function (e) {
+  $("input[id=input1], input[id=input2], input[id=input3], input[id=input4]").on("paste", function (e) {
     e.preventDefault();
   });
 });
@@ -78,32 +80,32 @@ function pasteValidater(event) {
     // 입력값이 16보다 크다면
     if (inputCharacter.length >= 16) {
       alert("1번 선택지");
-      $("input:first").val(inputCharacter.substr(0, 4));
-      $("input:eq(1)").val(inputCharacter.substr(4, 4));
-      $("input:eq(2)").val(inputCharacter.substr(8, 4));
-      $("input:last").val(inputCharacter.substr(12, 4));
+      $("input[id=input1]").val(inputCharacter.substr(0, 4));
+      $("input[id=input2]").val(inputCharacter.substr(4, 4));
+      $("input[id=input3]").val(inputCharacter.substr(8, 4));
+      $("input[id=input4]").val(inputCharacter.substr(12, 4));
       // 입력값이 16보다 작고, 12보다 크다면. 즉 13 ~ 15라면
     } else if (inputCharacter.length < 16 && inputCharacter.length > 12) {
       alert("2번 선택지");
-      $("input:first").val(inputCharacter.substr(0, 4));
-      $("input:eq(1)").val(inputCharacter.substr(4, 4));
-      $("input:eq(2)").val(inputCharacter.substr(8, 4));
-      $("input:last").val(inputCharacter.substr(12, 4));
+      $("input[id=input1]").val(inputCharacter.substr(0, 4));
+      $("input[id=input2]").val(inputCharacter.substr(4, 4));
+      $("input[id=input3]").val(inputCharacter.substr(8, 4));
+      $("input[id=input4]").val(inputCharacter.substr(12, 4));
       // 입력값이 12보다 작거나 같고, 8보다 크다면. 즉 9 ~ 12라면
     } else if (inputCharacter.length <= 12 && inputCharacter.length > 8) {
       alert("3번 선택지");
-      $("input:first").val(inputCharacter.substr(0, 4));
-      $("input:eq(1)").val(inputCharacter.substr(4, 4));
-      $("input:eq(2)").val(inputCharacter.substr(8, 4));
+      $("input[id=input1]").val(inputCharacter.substr(0, 4));
+      $("input[id=input2]").val(inputCharacter.substr(4, 4));
+      $("input[id=input3]").val(inputCharacter.substr(8, 4));
       // 입력값이 8보다 작거나 같고, 4보다 크다면. 즉 5 ~ 8라면
     } else if (inputCharacter.length <= 8 && inputCharacter.length > 4) {
       alert("4번 선택지");
-      $("input:first").val(inputCharacter.substr(0, 4));
-      $("input:eq(1)").val(inputCharacter.substr(4, 4));
+      $("input[id=input1]").val(inputCharacter.substr(0, 4));
+      $("input[id=input2]").val(inputCharacter.substr(4, 4));
       // 입력값이 4보다 작거나 같다면, 즉 4 이하라면
     } else if (inputCharacter.length <= 4) {
       alert("5번 선택지");
-      $("input:first").val(inputCharacter.substr(0, 4));
+      $("input[id=input1]").val(inputCharacter.substr(0, 4));
     }
   }
 
@@ -111,71 +113,72 @@ function pasteValidater(event) {
     // 입력값이 16보다 같거나 크다면. 즉 16 이상이라면
     if (inputCharacter.length >= 16) {
       alert("6번 선택지");
-      $("input:first").val(inputCharacter.substr(0, 4));
-      $("input:eq(1)").val(inputCharacter.substr(4, 4));
-      $("input:eq(2)").val(inputCharacter.substr(8, 4));
-      $("input:last").val(inputCharacter.substr(12, 4));
+      $("input[id=input1]").val(inputCharacter.substr(0, 4));
+      $("input[id=input2]").val(inputCharacter.substr(4, 4));
+      $("input[id=input3]").val(inputCharacter.substr(8, 4));
+      $("input[id=input4]").val(inputCharacter.substr(12, 4));
       // 입력값이 16보다 작고, 12보다는 크다면 즉. 13 ~ 15라면
     } else if (inputCharacter.length < 16 && inputCharacter.length > 12) {
       alert("7번 선택지");
-      $("input:eq(1)").val(inputCharacter.substr(0, 4));
-      $("input:eq(2)").val(inputCharacter.substr(4, 4));
-      $("input:last").val(inputCharacter.substr(8, 4));
+      $("input[id=input2]").val(inputCharacter.substr(0, 4));
+      $("input[id=input3]").val(inputCharacter.substr(4, 4));
+      $("input[id=input4]").val(inputCharacter.substr(8, 4));
       // 입력값이 12보다 같거나 작고, 8보다 크다면 즉 9 ~ 12라면
     } else if (inputCharacter.length <= 12 && inputCharacter.length > 8) {
       alert("8번 선택지");
-      $("input:eq(1)").val(inputCharacter.substr(0, 4));
-      $("input:eq(2)").val(inputCharacter.substr(4, 4));
-      $("input:last").val(inputCharacter.substr(8, 4));
+      $("input[id=input2]").val(inputCharacter.substr(0, 4));
+      $("input[id=input3]").val(inputCharacter.substr(4, 4));
+      $("input[id=input4]").val(inputCharacter.substr(8, 4));
       // 입력값이 8보다 같거나 작고, 4보다 크다면. 즉 5 ~ 8라면
     } else if (inputCharacter.length <= 8 && inputCharacter.length > 4) {
       alert("9번 선택지");
-      $("input:eq(1)").val(inputCharacter.substr(0, 4));
-      $("input:eq(2)").val(inputCharacter.substr(4, 4));
+      $("input[id=input2]").val(inputCharacter.substr(0, 4));
+      $("input[id=input3]").val(inputCharacter.substr(4, 4));
       // 입력값이 4보다 같거나 작다면, 즉 4 이하라면
     } else if (inputCharacter.length <= 4) {
       alert("10번 선택지");
-      $("input:eq(1)").val(inputCharacter.substr(0, 4));
+      $("input[id=input2]").val(inputCharacter.substr(0, 4));
     }
   }
 
   if (inputIndex == "3") {
     if (inputCharacter.length >= 16) {
       alert("11번 선택지");
-      $("input:first").val(inputCharacter.substr(0, 4));
-      $("input:eq(1)").val(inputCharacter.substr(4, 4));
-      $("input:eq(2)").val(inputCharacter.substr(8, 4));
-      $("input:last").val(inputCharacter.substr(12, 4));
+      $("input[id=input1]").val(inputCharacter.substr(0, 4));
+      $("input[id=input2]").val(inputCharacter.substr(4, 4));
+      $("input[id=input3]").val(inputCharacter.substr(8, 4));
+      $("input[id=input4]").val(inputCharacter.substr(12, 4));
     } else if (inputCharacter.length < 16 && inputCharacter.length > 12) {
       alert("12번 선택지");
-      $("input:eq(2)").val(inputCharacter.substr(0, 4));
-      $("input:last").val(inputCharacter.substr(4, 4));
+      $("input[id=input3]").val(inputCharacter.substr(0, 4));
+      $("input[id=input4]").val(inputCharacter.substr(4, 4));
     } else if (inputCharacter.length <= 12 && inputCharacter.length > 8) {
       alert("13번 선택지");
-      $("input:eq(2)").val(inputCharacter.substr(0, 4));
-      $("input:last").val(inputCharacter.substr(4, 4));
+      $("input[id=input3]").val(inputCharacter.substr(0, 4));
+      $("input[id=input4]").val(inputCharacter.substr(4, 4));
     } else if (inputCharacter.length <= 8 && inputCharacter.length > 4) {
       alert("14번 선택지");
-      $("input:eq(2)").val(inputCharacter.substr(0, 4));
-      $("input:last").val(inputCharacter.substr(4, 4));
+      $("input[id=input3]").val(inputCharacter.substr(0, 4));
+      $("input[id=input4]").val(inputCharacter.substr(4, 4));
     } else if (inputCharacter.length <= 4) {
       alert("15번 선택지");
-      $("input:eq(2)").val(inputCharacter.substr(0, 4));
+      $("input[id=input3]").val(inputCharacter.substr(0, 4));
     }
   }
 
   if (inputIndex == "4") {
     if (inputCharacter.length >= 16) {
       alert("16번 선택지");
-      $("input:first").val(inputCharacter.substr(0, 4));
-      $("input:eq(1)").val(inputCharacter.substr(4, 4));
-      $("input:eq(2)").val(inputCharacter.substr(8, 4));
-      $("input:last").val(inputCharacter.substr(12, 4));
+      $("input[id=input1]").val(inputCharacter.substr(0, 4));
+      $("input[id=input2]").val(inputCharacter.substr(4, 4));
+      $("input[id=input3]").val(inputCharacter.substr(8, 4));
+      $("input[id=input4]").val(inputCharacter.substr(12, 4));
     } else if (inputCharacter.length < 16) {
       alert("17번 선택지");
-      $("input:last").val(inputCharacter.substr(0, 4));
+      $("input[id=input4]").val(inputCharacter.substr(0, 4));
     }
   }
+
+  $("input[id=input4]").focus();
 }
 
-$("input:last").focus();
